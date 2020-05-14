e6bbbd861b8173bfd34df1bea8e24a86ef077144
Simone Vittori <hello@simonewebdesign.it>
2020-05-14 13:08:07 +0100

    Add files via upload

---
+++ elm-new/tools/chocolateyinstall.ps1
@@ -0,0 +1,7 @@
+﻿$version = '2.0.0'
+$url = "https://github.com/simonewebdesign/elm-new/archive/v$version.zip"
+$installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage 'elm-new' "$url" "$installDir"
+
+[Environment]::SetEnvironmentVariable("Path", $env:Path + ";$installDir\elm-new-$version", "Machine")
