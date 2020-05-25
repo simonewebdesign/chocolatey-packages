17fd730e7d4de5cd0aa9bf4ecc3eb710e90b32f9
Simone Vittori <hello@simonewebdesign.it>
2020-05-25 10:11:02 +0100

    Update elm-new installation script

---
+++ elm-new/tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$url = "https://github.com/simonewebdesign/elm-new/archive/v$env:chocolateyPackageVersion.zip"
+$installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage $env:ChocolateyPackageName $url $installDir
+
+Install-ChocolateyPath -PathToInstall "$installDir\$env:ChocolateyPackageName-$env:chocolateyPackageVersion"
