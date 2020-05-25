d47ee5b47bf3fe8d8de61ca33ea265149297f691
Simone Vittori <hello@simonewebdesign.it>
2020-05-25 10:11:22 +0100

    Add elm-new uninstall script

---
+++ elm-new/tools/chocolateyUninstall.ps1
@@ -0,0 +1,16 @@
+$installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$pathToRemove = "$installDir\$env:ChocolateyPackageName-$env:chocolateyPackageVersion"
+
+foreach ($path in [Environment]::GetEnvironmentVariable("PATH", "User").split(';'))
+{
+  If ($path)
+  {
+    If (($path -ine "$pathToRemove") -AND ($path -ine "$pathToRemove\"))
+    {
+      [string[]]$newpath += "$path"
+    }
+  }
+}
+$assembledNewPath = ($newpath -join(';')).trimend(';')
+
+[Environment]::SetEnvironmentVariable("PATH", $assembledNewPath, "User")
